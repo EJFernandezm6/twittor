88c49248abdba4cdfd29a6ab33fc76f54c96606a
EJFernandezm6 <ejfernandezm6@gmail.com>
2020-08-29 15:55:01 -0500

    primer

---
+++ js/sw-utils.js
@@ -0,0 +1,13 @@
+//Guardar caché dinámico
+function actualizarCacheDinamico(dynamicCache, req, res) {
+
+    if (res.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+
+}
